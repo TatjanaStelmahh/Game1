46ffe1aa5d09e1eddeeb28712a945d2e672b475b
tatjana stelmahh <tatjana.stelmahh@gmail.com>
2018-12-11 18:44:14 +0200

    player can jump on and kill enemies, enemies bump into player and kill him

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -1,9 +1,59 @@
-Quintus.ActionPlatformerPlayer = function(Q) {
+Quintus.ActionPlatformerEnemy = function(Q) {
+
+Q.component("commonEnemy", {
+    added: function() {
+        var entity = this.entity;
+        entity.on("bump.left, bump.right, bump.bottom", function(collision){
+            if(collision.obj.isA("Player")){
+                console.log('you died!');
+            }
+        });
+        entity.on("bump.top", function(collision){
+            if(collision.obj.isA("Player")) {
+                //little bounce on top of enemy
+                collision. obj.p.vy = -100;
+                //kill the enemy
+                this.destroy();
+            }
+        });
+    }
+});
 
     Q.Sprite.extend("GroundEnemy", {
         init: function(p) {
             this._super(p, {vx: -50, defaultDirection: "left"});
-            this.add("2d, aiBounce");
+            this.add("2d, aiBounce, commonEnemy");
+        }, 
+        step: function(dt) {
+            var dirX = this.p.vx / Math.abs(this.p.vx);
+            var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+            var nextElement = Q.stage().locate(this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+            var nextTile;
+
+            if(nextElement instanceof Q.TileLayer) {
+                nextTile = true;
+            }
+
+            //if enemy is on the ground and there is a cliff
+            if(!nextTile && ground){
+                if(this.p.vx > 0) {
+                    if(this.p.defaultDirection == "right"){
+                        this.p.flip = "x";
+                    }
+                    else{
+                        this.p.flip = false;
+                    }
+                }
+                else {
+                    if (this.p.defaultDirection == "left") {
+                        this.p.flip = "x";
+                    } 
+                    else {
+                        this.p.flip = false;
+                    }
+                }
+                this.p.vx = -this.p.vx;
+            }
         }
     });
 };
